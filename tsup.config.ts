d37626b63fb43b471d49ff7e58856efceda88061
jameswillis <james@wherobots.com>
2026-04-14 18:32:37 -0700

    add link to zarrita.js#409 in tsup config comment

--- tsup.config.ts
+++ tsup.config.ts
@@ -12,5 +12,6 @@ export default defineConfig({
   // Node-only modules (FileSystemStore, ZipFileStore, ReferenceStore) that
   // would otherwise break webpack consumers via node:buffer/node:fs imports.
   // numcodecs remains external to avoid bundling WASM codec implementations.
+  // See: https://github.com/manzt/zarrita.js/issues/409
   noExternal: ['zarrita', '@zarrita/storage'],
 })
